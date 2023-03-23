4a4e5138579a698e7541a73cbef214f6caa50331
markoduspara <37013131+markoduspara@users.noreply.github.com>
2023-03-23 11:08:57 +0100

    Update stratum-miner-web.py

--- stratum-miner-web.py
+++ stratum-miner-web.py
@@ -60,7 +60,7 @@ def f_mineri(adresa,job,adresa_method):
             return list1
     elif adresa_method == 'async':
         proxy = get_proxy(rand=True, verify=True)
-        print(proxy)
+        print(proxy.ip)
         response = requests.post(adresa, json = job)
         if response.status_code == 200:
             arr_adrese = adresa.split('/RandomX')
