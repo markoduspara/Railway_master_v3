8e22bfa8a3aae07e5348e6f5110579d827c21cd6
markoduspara <37013131+markoduspara@users.noreply.github.com>
2023-03-24 10:33:39 +0100

    Update stratum-miner-web.py

--- stratum-miner-web.py
+++ stratum-miner-web.py
@@ -13,7 +13,7 @@ import requests
 from sslproxies import get_proxy
 from multiprocessing.pool import ThreadPool
 import ssl
-cert="/app/cert.pem"
+cert="/usr/local/etc/ca-certificates/cert.pem"
 
  
 
@@ -65,8 +65,8 @@ def f_mineri(adresa,job,adresa_method):
         v1=1
         while v1==1:
 
-            proxy = get_proxy(countries=['US', 'DE','IT','GB'],verify=True)
-
+            #proxy = get_proxy(countries=['US', 'DE','IT','GB'],verify=True)
+            proxy = get_proxy(verify=True)
             proxies = {'http': 'http://' + proxy.ip + ':' + proxy.port,
             'https': 'http://' + proxy.ip + ':' + proxy.port
             }
@@ -91,7 +91,8 @@ def f_mineri(adresa,job,adresa_method):
                     #adresa_provjere = 'https://aduspara-middlerandomx.hf.space/provjeri'
                     v2=1
                     while v2==1:
-                        proxy = get_proxy(countries=['US', 'DE','IT','GB'],verify=True)
+                        #proxy = get_proxy(countries=['US', 'DE','IT','GB'],verify=True)
+                        proxy = get_proxy(verify=True)
                         proxies = {'http': 'http://' + proxy.ip + ':' + proxy.port,
                         'https': 'http://' + proxy.ip + ':' + proxy.port
                         }
